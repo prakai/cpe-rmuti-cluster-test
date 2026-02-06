26db58d6744add84d8d71531176adaa8eb589a21
Prakai Nadee <prakai.na@gmail.com>
2026-02-06 22:00:34 +0700

    Add success message and project info to index.php
    Replace phpinfo() with custom success message and project details.

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
-phpinfo();
+echo "<h1>Success!</h1>";
+echo "<h3>Project: $PROJECT is running with Nginx and PHP-FPM.</h3>";
+echo "<b>Server Hostname:</b> " . gethostname() . "<br>";
+echo "<b>URL Path:</b> /$PATH_NAME/<br>";
+?>
